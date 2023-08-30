ec8c407105ecadbd5634022ccc123889a409dd8d
Anshul Vishwakarma <72655193+AnshulWebDev@users.noreply.github.com>
2023-08-30 18:00:52 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-const a =554
+type Obj={
+    height:number;
+    weight:number;
+    gender?:boolean;
+}
+
+const obj:Obj={
+    height:3434,
+    weight:34,
+    gender:true
+}
+const obj2:Obj={
+    height:67,
+    weight:94,
+    gender:false
+}
