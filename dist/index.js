1b385618c37b3ea91d31143bd981fc7a91fc28ee
Anshul Vishwakarma <72655193+AnshulWebDev@users.noreply.github.com>
2023-09-15 20:08:28 +0530

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -4,10 +4,29 @@
 //     weight:number;
 //     gender?:boolean;
 // }
-const getData = (product) => {
-    console.log(product);
-};
-const product1 = {
-    name: "Mackbook", stock: 46, price: 9999, photo: "sample photo url"
-};
-getData(product1);
+// const obj:Obj={
+//     height:3434,
+//     weight:34,
+//     gender:true
+// }
+// const obj2:Obj={
+//     height:67,
+//     weight:94,
+//     gender:false
+// }
+// type FuncType=(n:number,m:number,l?:number)=>number
+// const func:FuncType=(n,m,l)=>{
+//     if(typeof l==="undefined") return n*m
+//     return n * m* l;
+// };
+// func(25,23)
+//function with object 
+// interface Product{name:String,stock:Number,price:Number,photo:String}
+// type GetDataType=(product:Product)=>void
+// const getData:GetDataType=(product)=>{
+//     console.log(product)
+// };
+// const product1:Product={
+//     name:"Mackbook",stock:46,price:9999,photo:"sample photo url"
+// };
+// getData(product1)
