a1c29a5a6efd921f06653c9b224f6f952b9e47ba
Anshul Vishwakarma <72655193+AnshulWebDev@users.noreply.github.com>
2023-09-15 19:07:55 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,38 @@
-type Obj={
-    height:number;
-    weight:number;
-    gender?:boolean;
-}
+// type Obj={
+//     height:number;
+//     weight:number;
+//     gender?:boolean;
+// }
 
-const obj:Obj={
-    height:3434,
-    weight:34,
-    gender:true
-}
-const obj2:Obj={
-    height:67,
-    weight:94,
-    gender:false
-}
+// const obj:Obj={
+//     height:3434,
+//     weight:34,
+//     gender:true
+// }
+// const obj2:Obj={
+//     height:67,
+//     weight:94,
+//     gender:false
+// }
 
-type FuncType=(n:number,m:number,l?:number)=>number
+// type FuncType=(n:number,m:number,l?:number)=>number
 
-const func:FuncType=(n,m,l)=>{
-    if(typeof l==="undefined") return n*m
-    return n * m* l;
+// const func:FuncType=(n,m,l)=>{
+//     if(typeof l==="undefined") return n*m
+//     return n * m* l;
+// };
+
+// func(25,23)
+
+//function with object 
+interface Product{name:String,stock:Number,price:Number,photo:String}
+type GetDataType=(product:Product)=>void
+const getData:GetDataType=(product)=>{
+    console.log(product)
 };
 
-func(25,23)
+const product1={
+    name:"Mackbook",stock:46,price:9999,photo:"sample photo url"
+};
+
+getData(product1)
